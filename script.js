82d4e162f6eb6a4254459c48f8ee872230debd7e
anageh577 <anageh577@gmail.com>
2021-03-19 14:32:39 +0200

    changes

--- script.js
+++ script.js
@@ -1,110 +1,74 @@
 var navbar = document.querySelector('menu')
-window.onscroll = function () {
-  // pageYOffset or scrollY
-  if (window.pageYOffset > 20) {
-    navbar.classList.add('scrolled')
-  } else {
-    navbar.classList.remove('scrolled')
-  }
+window.onscroll = function() {
+    // pageYOffset or scrollY
+    if (window.pageYOffset > 20) {
+        navbar.classList.add('scrolled')
+    } else {
+        navbar.classList.remove('scrolled')
+    }
 }
 
 filterSelection("all") // Execute the function and show all columns
 function filterSelection(c) {
-  if (c == 'all') {
-    document.getElementById('all').classList.add('current')
-    document.getElementById('solo').classList.remove('current')
-    document.getElementById('team').classList.remove('current')
-  } else if (c == 'webdesign') {
-    document.getElementById('all').classList.remove('current')
-    document.getElementById('solo').classList.add('current')
-    document.getElementById('team').classList.remove('current')
-  } else if (c == 'webdev') {
-    document.getElementById('all').classList.remove('current')
-    document.getElementById('solo').classList.remove('current')
-    document.getElementById('team').classList.add('current')
-  }
-  var x, i;
-  x = document.getElementsByClassName("project");
-  if (c == "all") {
-    console.log(''); c = "";
-  }
-  for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) {
-      addClass(x[i], "show")
-    };
-  }
+    if (c == 'all') {
+        document.getElementById('all').classList.add('current')
+        document.getElementById('solo').classList.remove('current')
+        document.getElementById('team').classList.remove('current')
+    } else if (c == 'solo') {
+        document.getElementById('all').classList.remove('current')
+        document.getElementById('solo').classList.add('current')
+        document.getElementById('team').classList.remove('current')
+    } else if (c == 'team') {
+        document.getElementById('all').classList.remove('current')
+        document.getElementById('solo').classList.remove('current')
+        document.getElementById('team').classList.add('current')
+    }
+    var x, i;
+    x = document.getElementsByClassName("project");
+    if (c == "all") {
+        console.log('');
+        c = "";
+    }
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(c) > -1) {
+            addClass(x[i], "show")
+        };
+    }
 }
 
 // Show filtered elements
 function addClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
     }
-  }
 }
 
 // Hide elements that are not selected
 function removeClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
     }
-  }
-  element.className = arr1.join(" ");
+    element.className = arr1.join(" ");
 }
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("btnWrapper");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function () {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
-
-
-// this is to control the slide modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
 }
